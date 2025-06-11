584df4a5bb0c50fb8c4925771b67f97bed0eaec3
PrashantaDA <apras.dixit@gmail.com>
2025-06-11 15:06:16 +0545

    Added new project

--- src/assets/sites/index.jsx
+++ src/assets/sites/index.jsx
@@ -2,5 +2,6 @@ import Gamexrip from "./images/Gamexrip.png";
 import Rexipe from "./images/Rexipe.png";
 import Pokedex from "./images/Pokedex.png";
 import Hotelix from "./images/Hotelix.png";
+import ModRestro from "./images/ModRestro.png";
 
-export { Gamexrip, Rexipe, Pokedex, Hotelix };
+export { Gamexrip, Rexipe, Pokedex, Hotelix, ModRestro };
